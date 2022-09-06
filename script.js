43810c5762dfc966de899bf79221c179cd7ccc18
mahxmah02 <mahjoubixmahmoud02@gmail.com>
2022-09-06 19:38:26 +0100

    quan change,heart, deleting

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-var pbtn=document.querySelectorAll(".plus-btn")
+var pbtn=document.querySelectorAll(".plus-btn");
 var qant=document.querySelectorAll(".Quant");
+var mbtn=document.querySelectorAll(".minus-btn");
+var like=document.querySelectorAll(".like");
+var dele=document.querySelectorAll(".delete");
+
 for (let i = 0; i < pbtn.length; i++) {
 pbtn[i].addEventListener('click',()=>qant[i].value=parseInt(qant[i].value)+1 );
 }
-
-var mbtn=document.querySelectorAll(".minus-btn")
-var qant=document.querySelectorAll(".Quant");
-for (let i = 0; i < pbtn.length; i++) {
-mbtn[i].addEventListener('click',()=>qant[i].value=parseInt(qant[i].value)+1 );
+for (let i = 0; i < mbtn.length; i++) {
+mbtn[i].addEventListener('click',()=>{
+    if(parseInt(qant[i].value)>0){
+        qant[i].value=parseInt(qant[i].value)-1 
+    }
+   
 }
-var fp=document.querySelector("#finalPrice") 
-var price=document.getElementsByClassName('price')
-for (let i = 0; i < qant.length; i++) 
+)
+}
+
+
+//like button
+for (let i=0;i< like.length;i++)
 {
-    fp.value+=qant[i].value*parseInt(price[0].innerHTML);
+    like[i].addEventListener('click',()=>{
+        if(like[i].style.color == "black"){
+            like[i].style.color="red"
+        }
+        else{
+            like[i].style.color="black"
+        }
+    })
+
+}
+for (let i=0 ; i<dele.length;i++)
+{
+    dele[i].addEventListener('click',()=>
+        {
+            dele[i].parentNode.remove()       }
+    )
 }
 
  
